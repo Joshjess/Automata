0be4db95a19b6aa7f812f04850e86e4610647cec
J.J.A. Offermans <joshua.offermans@hva.nl>
2018-02-14 10:39:38 +0100

    final

--- untitled2/src/com/company/Main.java
+++ untitled2/src/com/company/Main.java
@@ -1,65 +1,106 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-            EvenNumber even  = new GumBallMachine("aba");
+            new GumBallMachine();
     }
 
-    public class GumBallMachine {
-        SoldOut soldOut;
-        NoQuarter noQuarter;
-        HasQuarter hasQuarter;
-        GumballSold gumballSold;
+    public static class GumBallMachine {
+        private int Amount_GumBalls = 0;
 
-        public GumBallMachine(String s) {
-            /* state_s.setStates(state_q);
-            state_q.setStates(state_s);
-            state_s.setWord(s); */
+        public GumBallMachine() {
+            new SoldOut();
+        }
 
-            soldOut =  new Soldout();
-            noQuarter = new NoQuarter();
-            hasQuarter = new HasQuarter();
-            gumballSold = new Gumballsold();
+
+
+        class SoldOut {
+
+
+            public SoldOut() {
+                fill();
+                new NoQuarter();
+            }
+
+            public void fill(){
+                Amount_GumBalls = 5;
+                System.out.print("Gumballs filled \n");
+            }
+
+
 
         }
 
-        public void setAccept(boolean accept) {
-            if (accept == true) System.out.println("Accepted");
-            if (accept == false) System.out.println("Not accepted");
-            System.exit(1);
-        }
-    }
+        class NoQuarter {
 
-    public class SoldOut {
-        NoQuarter noQuarter;
-        EvenNumber evenNumber;
-        State_Q state_q;
-        String restWord;
+            public NoQuarter(){
+                InsertQuarter();
+            }
 
-        public State_S(EvenNumber evenNumber){
-            this.evenNumber =evenNumber;
-        }
+            public HasQuarter InsertQuarter(){
+                System.out.print("press c to insert coin \n");
+                Scanner scan = new Scanner(System.in);
+                String coin = scan.next();
+                return new HasQuarter(coin);
+            }
 
-        public void setStates(State_Q state_q){
-            this.state_q = state_q;
+
+
         }
 
-        public void gettingA() {
-            state_q.setWord(restWord);
+
+        class HasQuarter{
+
+            public HasQuarter(String coin){
+                if (coin.equals("c")){
+                    turnCrank();
+                }
+                else{
+                    ejectQuarter();
+                }
+            }
+
+            public GumballSold turnCrank(){
+                System.out.print("Turning Crank \n");
+                return new GumballSold();
+            }
+
+            public NoQuarter ejectQuarter(){
+                System.out.print("wrong coin \n");
+                return new NoQuarter();
+            }
+
         }
 
-        public void gettingB(){
-            this.setWord(restWord);
+
+        class GumballSold{
+
+            public GumballSold(){
+                    dispense();
+            }
+
+            public void dispense(){
+                System.out.print("Dispensing Gumball \n");
+                Amount_GumBalls--;
+                System.out.print("Amount gumballs "+ Amount_GumBalls + "\n");
+                if (Amount_GumBalls > 0){
+                    new NoQuarter();
+                }
+
+                else{
+                    new SoldOut();
+                }
+            }
+
+
         }
 
-        public void setWord(String s){
-        if(s.equals(""))evenNumber.setAccept(true);
-        String character = s.substring(0, 1);
-        restWord =s.substring(1,s.length());
-        if(character.equals("a")) gettingA();
-        if(character.equals("b")) gettingB();
-        }
+
+
+
     }
 
 }
